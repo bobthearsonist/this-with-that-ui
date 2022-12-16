004b850e0a7f624084b69836dba971d53984359a
Martin Perry <mperr01@gmail.com>
2022-12-16 11:11:26 -0500

    working tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <BrowserRouter>
+      <App />{' '}
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/materials/i);
   expect(linkElement).toBeInTheDocument();
 });
