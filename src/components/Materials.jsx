f7c4d475cfda28ce8a395e2e617d767ae7486cb9
Martin Perry <mperr01@gmail.com>
2022-12-19 15:25:40 -0500

    working grid

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Material from './Material';
 
 export default function Materials(props) {
   return (
     <React.Fragment>
       <div>{props.data.length} Materials</div>
+      <div className="col">
+        {props.data.map((material) => {
+          return <Material name={material.Name} key={material.Name} />;
+        })}
+      </div>
     </React.Fragment>
   );
 }
