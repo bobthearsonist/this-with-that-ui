a297b5d341812aba00106b0b6c1d3ddf8b1f03ae
Martin Perry <mperr01@gmail.com>
2022-12-19 11:53:24 -0500

    add some very basic state

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -1,7 +1,22 @@
 import React, { PureComponent } from 'react';
 
 export default class Materials extends PureComponent {
+  state = {
+    materials: [],
+  };
+
+  componentDidMount() {
+    const rehydratedState = localStorage.getItem('materialsData');
+    if (rehydratedState) {
+      this.setState({ materials: JSON.parse(rehydratedState) });
+    }
+  }
+
   render() {
-    return <div>Materials</div>;
+    return (
+      <React.Fragment>
+        <div>{this.state.materials.length} Materials</div>
+      </React.Fragment>
+    );
   }
 }
