27e4e62efb742a5b9edd5237dc80ece0cbf6b059
Martin Perry <mperr01@gmail.com>
2022-12-20 08:37:57 -0500

    add a button

--- src/components/Material.jsx
+++ src/components/Material.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-export default function Material({ name, key }) {
+export default function Material({ name, key, msds_id, onItemSelect }) {
   return (
-    <div className="container">
-      <div className="row" key={key}>
-        {name}
+    <React.Fragment>
+      <div className="row">
+        <div className="col" key={key}>
+          {name}
+        </div>
+        <div className="col" key={key + msds_id}>
+          <button
+            type="button"
+            className="btn-outline-secondary btn-sm"
+            onClick={() => onItemSelect()}
+          >
+            {'info'}
+          </button>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -3,13 +3,10 @@ import Material from './Material';
 
 export default function Materials(props) {
   return (
-    <React.Fragment>
-      <div>{props.data.length} Materials</div>
-      <div className="col">
-        {props.data.map((material) => {
-          return <Material name={material.Name} key={material.Name} />;
-        })}
-      </div>
-    </React.Fragment>
+    <div className="container-fluid">
+      {props.data.map((material) => {
+        return <Material name={material.Name} key={material.Name} />;
+      })}
+    </div>
   );
 }
