d4f90809f85656ab45e5a8506f94205fcc93417d
Martin Perry <mperr01@gmail.com>
2022-12-20 09:58:52 -0500

    add navigation back to materials

--- src/components/MaterialInfo.jsx
+++ src/components/MaterialInfo.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const MaterialInfo = () => {
+  const navigate = useNavigate();
+
   //somewhat confusing, that to get state you use location when passing it through the router...
   //   const [material, setMaterial] = useState();
   const location = useLocation();
   const material = location.state;
 
+  const handleBackToMaterials = () => {
+    navigate('/materials');
+  };
+
   return (
     <React.Fragment>
       <div>{material.Name} Info</div>
+      <div className="d-grid gap-2">
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={handleBackToMaterials}
+        >
+          Back to Materials
+        </button>
+      </div>
     </React.Fragment>
   );
 };
