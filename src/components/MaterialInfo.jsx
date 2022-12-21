c3debcfcda387390b5784d812326041a009733ef
Martin Perry <mperr01@gmail.com>
2022-12-21 09:44:13 -0500

    open MSDS links

--- src/components/MaterialInfo.jsx
+++ src/components/MaterialInfo.jsx
@@ -7,15 +7,15 @@ export const MaterialInfo = () => {
   //somewhat confusing, that to get state you use location when passing it through the router...
   //   const [material, setMaterial] = useState();
   const location = useLocation();
-  const material = location.state;
+  const { MSDS, ...material } = location.state;
 
   const handleBackToMaterials = () => {
     navigate('/materials');
   };
 
-  Object.keys(material).forEach((key, index) => {
-    console.log(key, material[key]);
-  });
+  const handleMsds = () => {
+    window.open(MSDS);
+  };
 
   return (
     <React.Fragment>
@@ -35,6 +35,9 @@ export const MaterialInfo = () => {
         })}
       </div>
       <div className="d-grid gap-2">
+        <button className="btn btn-primary" type="button" onClick={handleMsds}>
+          MSDS
+        </button>
         <button
           className="btn btn-primary"
           type="button"
